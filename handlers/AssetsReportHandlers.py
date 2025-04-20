30bb7774691ab695f80d4e5516a1d3951a663c58
HellFinger-s <62233619+HellFinger-s@users.noreply.github.com>
2025-04-20 11:08:34 +0300

    Добавлено: сообщение пользователю при отсутствии информации

--- handlers/AssetsReportHandlers.py
+++ handlers/AssetsReportHandlers.py
@@ -28,36 +28,38 @@ async def get_analysis(message: types.Message, state: FSMContext):
             async with session.get(request_url, params=params) as response:
                 if response.status == 200:
                     data = await response.json()
+                    if len(data) == 0:
+                        text = "Список отслеживаемых пар пуст"
+                    else:
+                        for index in range(len(data)):
+                            request_url = base_url + "currency-pair/rate"
+                            params = {
+                                "Cur1": data[index]["currency1"],
+                                "Cur2": data[index]["currency2"]
+                            }
 
-                    for index in range(len(data)):
-                        request_url = base_url + "currency-pair/rate"
-                        params = {
-                            "Cur1": data[index]["currency1"],
-                            "Cur2": data[index]["currency2"]
-                        }
+                            async with aiohttp.ClientSession() as session:
+                                try:
+                                    async with session.get(request_url, params=params) as response:
+                                        if response.status == 200:
+                                            rate_data = await response.json()
+                                            print(rate_data)
+                                            text += str(index + 1) + ". " + data[index]["currency1"] + "|" + \
+                                                    data[index]["currency2"] + "  " + str(rate_data) + "\n"
+                                        else:
+                                            text = "Произошла ошибка, попробуйте позже"
+                                            await message.answer(text, reply_markup=keyboards.get_currency_kb())
+                                            await state.set_state(CurrencyStates.IN_CURRENCY)
+                                            return
 
-                        async with aiohttp.ClientSession() as session:
-                            try:
-                                async with session.get(request_url, params=params) as response:
-                                    if response.status == 200:
-                                        rate_data = await response.json()
-                                        print(rate_data)
-                                        text += str(index + 1) + ". " + data[index]["currency1"] + "|" + \
-                                                data[index]["currency2"] + "  " + str(rate_data) + "\n"
-                                    else:
-                                        text = "Произошла ошибка, попробуйте позже"
-                                        await message.answer(text, reply_markup=keyboards.get_currency_kb())
-                                        await state.set_state(CurrencyStates.IN_CURRENCY)
-                                        return
-
-                            except Exception as e:
-                                print(str(e))
-                                text = "Произошла ошибка подключения, попробуйте позже"
-                                await message.answer(text, reply_markup=keyboards.get_currency_kb())
-                                await state.set_state(CurrencyStates.IN_CURRENCY)
-                                return
+                                except Exception as e:
+                                    print(str(e))
+                                    text = "Произошла ошибка подключения, попробуйте позже"
+                                    await message.answer(text, reply_markup=keyboards.get_currency_kb())
+                                    await state.set_state(CurrencyStates.IN_CURRENCY)
+                                    return
                 else:
-                    text = "Произошла ошибка, попробуйте позже"
+                    text = "Произошла ошибка, возможно список отслеживаемых пар пуст"
         except Exception as e:
             print(str(e))
             text = "Произошла ошибка подключения, попробуйте позже"
